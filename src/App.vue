47dfd8ed88b7e0854b00db124a149e0bcd5d2aa9
sferreira <sferreir@bu.edu>
2023-09-04 20:03:20 -0400

    add modaldisplay

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1> {{ title }}</h1>
+  <!-- <input type="text" ref="name">
+  <button @click="handleClick">Submit</button> -->
+  <div v-if="showModal">
+    <ModalDisplay :header="header" :text="text" :theme="theme" @close="toggleModal">
+      <h2>Slot heading</h2>
+      <p>Slot paragraph</p>
+      <template v-slot:links>
+        <a href="">Sign up</a>
+        <a href="">More Info</a>
+      </template>
+      <template v-slot:modalTwo>
+        <h3>Modal Two</h3>
+      </template>
+    </ModalDisplay>
+  </div>
+  <div v-if="showModalTwo">
+    <ModalDisplay @close="toggleModalTwo">
+      <template v-slot:modalTwo>
+        <h1>Modal Two</h1>
+      </template>
+    </ModalDisplay>
+  </div>
+  <button @click="toggleModal">Show Modal</button>
+  <button @click="toggleModalTwo">Show Modal 2.0</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ModalDisplay from './components/ModalDisplay.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { ModalDisplay },
+  data() {
+    return {
+      title: 'My first app',
+      header: 'Sign up for a giveaway',
+      text: 'Grab your ninja swag...',
+      theme: 'sale',
+      prize: 'scooter',
+      showModal: false,
+      showModalTwo: false
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    },
+    toggleModal(){
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo(){
+      this.showModalTwo = !this.showModalTwo
+    },
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
+// loaded first and creates the Vue object.
 import { createApp } from 'vue'
+// imports the Vue default component.
 import App from './App.vue'
 
+import './assets/global.css'
+
+// injects the default component into the html app element. 
 createApp(App).mount('#app')
